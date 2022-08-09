e6816e282c273cdd278426967a8a42cf2053111e
juy4556 <kws05050@naver.com>
2022-08-09 16:50:47 +0900

    Feat: ConnectLikeAPI

--- src/screens/Detail_Pose.js
+++ src/screens/Detail_Pose.js
@@ -1,11 +1,19 @@
-import React, {useEffect, useState, useContext} from 'react';
-import { StyleSheet, Text, View, StatusBar, TouchableOpacity, Image, ImageBackground, Dimensions } from 'react-native';
+import React, { useEffect, useState, useContext, useCallback } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TouchableOpacity,
+  Image,
+  ImageBackground,
+  Dimensions,
+} from "react-native";
 import AntDesign from "react-native-vector-icons/AntDesign";
 import { UserContext } from "../contexts/User";
 import axios from "axios";
 
 export default function Detail_Pose({ route }) {
-
   const devWidth = Dimensions.get("window").width;
   const devHeight = Dimensions.get("window").height;
 
@@ -16,7 +24,7 @@ export default function Detail_Pose({ route }) {
   const [img, setImg] = useState();
   const [likeCnt, setLikeCnt] = useState();
   const [views, setViews] = useState();
-  const [isLike, setIsLike] = useState(false);
+  const [isLike, setIsLike] = useState(0);
   const { dispatch, user } = useContext(UserContext);
 
   useEffect(() => {
@@ -24,25 +32,25 @@ export default function Detail_Pose({ route }) {
       axios({
         method: "get",
         headers: {
-          'x-access-token': `${user?.token}`
+          "x-access-token": `${user?.token}`,
         },
         url: `http://13.125.249.247/filme/pose/${idx}`,
       })
-          .then(function (response) {
-            const result = response.data;
-            console.log(result);
-            setUserIdx(result[0].idx);
-            setProfile(result[0].profileURL);
-            setNickname(result[0].name);
-            setImg(result[0].imageURL);
-            setLikeCnt(result[0].likeCnt);
-            setViews(result[0].views);
-            setIsLike(result[0].isLike);
-
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
+        .then(function (response) {
+          const result = response.data;
+          console.log(result);
+          setUserIdx(result[0].idx);
+          setProfile(result[0].profileURL);
+          setNickname(result[0].name);
+          setImg(result[0].imageURL);
+          setLikeCnt(result[0].likeCnt);
+          setViews(result[0].views);
+          setIsLike(result[0].isLike);
+          console.log(isLike);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     } catch (e) {
       console.log(e);
       alert("Error", e);
@@ -50,19 +58,46 @@ export default function Detail_Pose({ route }) {
     }
   }, []);
 
+  const _handleLikePress = useCallback(async () => {
+    try {
+      axios({
+        method: "post",
+        url: "http://13.125.249.247/filme/like/" + idx,
+        headers: {
+          "x-access-token": `${user?.token}`,
+        },
+      })
+        .then(function (response) {
+          if (isLike == 0) {
+            setIsLike(1);
+            setLikeCnt(likeCnt + 1);
+          } else {
+            setIsLike(0);
+            setLikeCnt(likeCnt - 1);
+          }
+          return response.data;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    } catch (e) {
+      console.log(e);
+      alert("Error", e);
+    } finally {
+    }
+  }, [user, idx, setIsLike, isLike, setLikeCnt, likeCnt]);
+
   return (
     <View style={styles.container}>
-      <StatusBar backgroundColor='white' barStyle='dark-content' />
+      <StatusBar backgroundColor="white" barStyle="dark-content" />
 
       <View style={styles.header}>
-        <Text style={{ fontSize: 22, fontWeight: 'bold' }}>포즈자랑</Text>
+        <Text style={{ fontSize: 22, fontWeight: "bold" }}>포즈자랑</Text>
       </View>
 
       <View style={styles.profileSection}>
         <TouchableOpacity>
-          <Image
-            source={{uri: `${profile}`}}
-            style={styles.profile} />
+          <Image source={{ uri: `${profile}` }} style={styles.profile} />
         </TouchableOpacity>
         <Text style={styles.nickname}>{nickname}</Text>
       </View>
@@ -70,58 +105,65 @@ export default function Detail_Pose({ route }) {
       <View style={styles.photoSection}>
         <ImageBackground
           source={require("../../storage/images/detail-pose.png")}
-          style={{ height: devHeight - 270, width: devWidth - 20, alignItems: 'center', justifyContent: 'center' }}>
-          <Image source={{uri: img}} style={styles.img} />
+          style={{
+            height: devHeight - 270,
+            width: devWidth - 20,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Image source={{ uri: img }} style={styles.img} />
         </ImageBackground>
       </View>
 
       <View style={styles.likesSection}>
-        <View style={{ flexDirection: 'row' }}>
-          <TouchableOpacity onPress={() => setIsLike(!isLike)}>
+        <View style={{ flexDirection: "row" }}>
+          <TouchableOpacity>
             <AntDesign
               name={isLike ? "heart" : "hearto"}
-              style={[{ color: isLike ? 'tomato' : 'black' }, styles.heart]} />
+              style={[{ color: isLike ? "tomato" : "black" }, styles.heart]}
+              onPress={_handleLikePress}
+            />
           </TouchableOpacity>
-          <Text>{isLike ? likeCnt + 1 : likeCnt}개</Text>
+          <Text>{likeCnt}개</Text>
         </View>
         <Text>조회수 {views}회</Text>
       </View>
-
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   header: {
-    width: '100%',
+    width: "100%",
     height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   profileSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     marginTop: 25,
   },
   profile: {
     width: 50,
     height: 50,
     borderRadius: 100,
-    borderColor: '#AAAAAA',
+    borderColor: "#AAAAAA",
     borderWidth: 3,
   },
   nickname: {
     fontSize: 15,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     margin: 10,
   },
   photoSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   img: {
     width: 150,
@@ -129,8 +171,8 @@ const styles = StyleSheet.create({
     marginTop: 30,
   },
   likesSection: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginHorizontal: 30,
   },
   heart: {
