867334eb9d7ac900eb2abceb4e7b73898f255415
ji-mango <susie000301@naver.com>
2022-08-10 18:12:12 +0900

    Fix: stamp rendering

--- src/screens/MyPage.js
+++ src/screens/MyPage.js
@@ -259,7 +259,7 @@ export default function MyPage({navigation}) {
             alert(e);
         } finally {
         }
-    }, [user, totalstamp]);
+    }, [user, stamp, setStamp, totalstamp]);
 
     return (
         <View style={styles.container}>
